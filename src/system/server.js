8609e590336cd318aef9d9c10b8c95dd73c1cdef
tiburilloma_sd2023@CEACCS.NET <tiburilloma_sd2023@CEACCS.NET>
2019-11-12 16:41:44 +0800

    updated

--- src/system/server.js
+++ src/system/server.js
@@ -7,10 +7,10 @@ const bodyParser = require('body-parser');
 //models
 const Service = require('./Services');
 const Appointment = require('./Appointment');
+const Admin = require('./Account');
 
 //database - mongoose
 mongoose.connect('mongodb://localhost:27017/DbToothbook', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
-
 mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
 mongoose.set('useFindAndModify', false);
 
@@ -18,13 +18,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-//Services - Admin side
+//API -> Services - Admin side
 app.get('/api/service/list', (req, res) => {
-    Service.find({}, (err, services) => {
+    Service.find({}, (err, data) => {
         if (err) {
             return res.status(404).send('Error while getting list of services!');
         }
-        return res.send({ services })
+        return res.send({ data })
     })
 })
 
@@ -39,24 +39,24 @@ app.post('/api/service/create', (req, res) => {
 
 app.post('/api/service/update/:id', (req, res) => {
     console.log(req.body)
-    Service.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, service) => {
+    Service.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, data) => {
         if (err) return res.status(404).send({ message: err.message });
-        return res.send({ message: 'Service is successfully updated', service })
+        return res.send({ message: 'Service is successfully updated', data })
     })
 })
 
 app.post('/api/service/delete/:id', (req, res) => {
-    Service.findByIdAndRemove(req.params.id, (err, service) => {
+    Service.findByIdAndRemove(req.params.id, (err, data) => {
         if (err) return res.status(404).send({ message: err.message });
-        return res.send({ message: 'Service is successfully deleted!', service })
+        return res.send({ message: 'Service is successfully deleted!', data })
     })
 })
 
-// Appointment
+//API -> Appointment
 app.get('/api/appointment/list', (req, res) => {
-    Appointment.find({}).exec((err, appointment) => {
+    Appointment.find({}).exec((err, data) => {
         if (err) return res.status(404).send('Error while getting list of appointment!');
-        return res.send({ appointment })
+        return res.send({data})
     })
 })
 
@@ -78,18 +78,21 @@ app.post('/api/appointment/create', (req, res) => {
 });
 
 app.post('/api/appointment/update/:id', (req, res) => {
-    Appointment.findByIdAndUpdate(req.params.id, req.body.status, { new: true }, (err, appointment) => {
+    Appointment.findByIdAndUpdate(req.params.id, {status:req.body.status} , { new: true }, (err, data) => {
         if (err) return res.status(404).send({ message: err.message });
-        return res.send({ message: 'Service is successfully updated', appointment })
+        return res.send({ message: 'Service is successfully updated', data })
     })
 })
 
 app.post('/api/appointment/delete/:id', (req, res) => {
-    Appointment.findByIdAndRemove(req.params.id, (err, appointment) => {
+    Appointment.findByIdAndRemove(req.params.id, (err, data) => {
         if (err) return res.status(404).send({ message: err.message });
-        return res.send({ message: 'Service is successfully deleted!', appointment })
+        return res.send({ message: 'Service is successfully deleted!', data })
     })
 })
+
+//Admin
+
 
 
 const PORT = 3000;
